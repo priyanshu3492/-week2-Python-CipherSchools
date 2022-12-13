80b219abc0a39f6500e3b37611ebf6505f57f358
Priyanshu Ranjan <115810094+priyanshu3492@users.noreply.github.com>
2022-12-13 20:38:46 +0530

    Add files via upload

---
+++ Class 117-Intro to dictionaries-CipherSchools.py
@@ -0,0 +1,26 @@
+# dictonaries intro
+
+# how to create dictonaries
+# step 1
+user = {'name':'harsh','age': 24}
+# print(user)
+# print(type(user))
+user1=dict(name='Harsh', age= 24)
+# print(user1)
+# how to acess data from dictonary
+# print(user['name'])
+# print(user['age'])
+# which type of data can dictonary store?
+# anything
+
+user_info={
+    'name':'harshit',
+    'age': 24,
+    'fav_movies': ['coco'],
+    'fav_tunes': ['awakening','fairy tale']}
+# print(user_info['fav_tunes'])
+
+# How to add data to empty dictonary
+user_info2= {}
+user_info2['name']='mohit'
+print(user_info2)
---
+++ Class 122-More about get() Method-CipherSchools.py
@@ -0,0 +1,4 @@
+# more about get method
+user= {'name' : 'priyanshu', 'age' : '19', 'age' : '85'}
+# print(user.get('names','not found'))
+print(user)
